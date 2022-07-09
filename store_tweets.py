a30d6fc3ce01c053ede3d93aedd3c287bd68008c
Vlad Zahrevskiy <vzvl.dos@gmail.com>
2022-07-09 13:45:30 +0300

    Exclude rule building, as it is not ready

--- store_tweets.py
+++ store_tweets.py
@@ -16,26 +16,12 @@ class MongoStorer(tweepy.StreamingClient):
 		self._db['tweets'].insert_one(dict(tweet))
 
 
-def rule_len(infls):
-	return 9 * len(infls) - 4 + sum(len(infl) for infl in infls)
-
-
-def build_rules(infls):
-	while len(infls):
-		n = 1
-		first_n = infls[:n]
-		while not (rule_len(first_n) >= 512 or len(infls) < n):
-			n += 1
-			first_n = infls[:n]
-		yield ' OR '.join(f'from:{infl}' for infl in first_n[:-1])
-		infls = infls[(n-1):]
-
-
 def run():
 	mongo = MongoClient("mongodb://127.0.0.1:27017/")
 	db = mongo['twitter-collector']
 
 	storer = MongoStorer(db, os.environ['API_TOKEN'])
+	storer.filter()
 
 
 if __name__ == '__main__':
