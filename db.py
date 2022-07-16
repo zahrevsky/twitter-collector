c89b86010b1c3b2bbdfe5e3799bc1efb7b538e9b
Vlad Zahrevskiy <vzvl.dos@gmail.com>
2022-07-16 11:54:44 +0300

    Introduce tweet dumper tool

--- db.py
+++ db.py
@@ -5,3 +5,7 @@ def tweet_storage():
     mongo = MongoClient("mongodb://127.0.0.1:27017/")
     db = mongo['twitter-collector']
     return db
+
+
+def all_tweets():
+    return tweet_storage()['tweets'].find()
---
+++ dump_tweets.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+from db import all_tweets
+from env import env
+
+
+def dump_tweets(filename):
+    to_dump = {
+        'dumped_at': datetime.now().isoformat(),
+        'data': [tweet for tweet in all_tweets()]
+    }
+
+    with open(filename, 'w') as out:
+        json.dump(to_dump, out, default=str)
+
+
+if __name__ == '__main__':
+    dump_tweets(env('DUMP_PATH'))
