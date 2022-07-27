bd3dbc0d28f6b78cc587d3a138506d4845c907aa
Vlad Zahrevskiy <vzvl.dos@gmail.com>
2022-07-27 17:59:43 +0300

    Add a tool to retrieve authors for stored tweets

---
+++ enrich_authors.py
@@ -0,0 +1,40 @@
+from tweepy import Client
+from tqdm import tqdm
+
+from db import tweet_storage
+from env import env
+
+
+def enrich_authors():
+    strg = tweet_storage()
+    client = Client(env('API_TOKEN'))
+
+    already_dumped = 0
+    unretrievable = 0
+    total = 0
+    for tweet in tqdm(strg['tweets'].find()):
+        total += 1
+        if 'author_id' not in tweet:
+            try:
+                author = client.get_tweet(
+                    tweet['id'],
+                    tweet_fields=['author_id']).data['author_id'
+                ]
+                strg['tweets'].update_one(
+                    {'id': tweet['id']}, 
+                    {'$set': {'author_id': author}}
+                )
+            except Exception:
+                unretrievable += 1
+        else:
+            already_dumped += 1
+
+    print(
+        f"Processed {total} tweets, " 
+        f"{already_dumped} already have author, "
+        f"{unretrievable} failed to retrieve"
+    )
+
+
+if __name__ == '__main__':
+    enrich_authors()
